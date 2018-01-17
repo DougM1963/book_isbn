ed38559b88a51b378eca969967a2e6ff6483e199
dougm1963 <dougmeador1963@gmail.com>
2018-01-17 12:02:26 -0500

    start up of testing

---
+++ bookisbn.rb
@@ -0,0 +1,6 @@
+def isbn(bname)
+   isbn_hash = {"Harry Potter Chamber of Secrets" => "0439064872", "Wizard of Oz" => "0345335902", "Jaws" => "0345544145", 
+    	"Adventures of Tom Sawer" => "1441755314", "Cat in the Hat" => "9780394800011"}
+    
+isbn_hash[bname]
+en
---
+++ test.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require_relative "bookisbn.rb"
+
+class Testnamegame < Minitest::Test 
+
+	def test_assert_that_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_assert_Harry_potter_chamber_of_secrets_returns_0439064872
+		assert_equal("0439064872", bname("Harry Potter Chamber of Secrets"))
+	end
+
+    def test_assert_Jaws_returns_0345544145
+		assert_equal("0345544145", bname("Jaws"))
+	end
+
+	def test_assert_Wizard_of_Oz_returns_0345335902
+		assert_equal("0345335902", bname("Wizard of Oz"))
+	end
