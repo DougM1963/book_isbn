e332789b07b44b348ed1f58b322d75926cc8fe36
dougm1963 <dougmeador1963@gmail.com>
2018-01-18 19:44:08 -0500

    finished produce

--- bookisbn.rb
+++ bookisbn.rb
@@ -8,8 +8,8 @@ def picture(bname)
     "Adventures of Tom Sawer" => "img//tom_resized.jpg", "Cat in the Hat" => "img//cat_in_the_hat_resized.jpg"}
 picture_hash[bname]
 end
-def number(isbn_num)
-   book_hash = {"0439064872" => "Harry Potter Chamber of Secrets", "0345335902" => "Wizard of Oz", "0345544145" => "Jaws", 
-    	"1441755314" => "Adventures of Tom Sawer", "9780394800011" => "Cat in the Hat"}   
+def isbn_n(isbn_num)
+	 book_hash = {"Harry Potter Chamber of Secrets" => "0439064872", "Wizard of Oz" => "0345335902", "Jaws" => "0345544145", 
+    "Adventures of Tom Sawer" => "1441755314", "Cat in the Hat" => "9780394800011"}
 book_hash[isbn_num]
 end
